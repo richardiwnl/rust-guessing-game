705aaa34516ae545d55203eaca19cb67e8739bc4
richardcss <richardiwnl@outlook.com>
2022-06-14 15:39:24 -0300

    handling errors

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please, input a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
