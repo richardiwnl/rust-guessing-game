6f67a1ec1b2126b43c06f7512ed6e9f55e38a807
richardcss <richardiwnl@outlook.com>
2022-06-14 15:31:35 -0300

    generating a random secret number

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
 
     println!("Guess the number!");
 
     let mut guess: String = String::new();
+    let secret_number: u32 = rand::thread_rng().gen_range(0..=100);
+
+    println!("The secret number is: {}", secret_number);
+    println!("Please, input a number: ");
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
 
     println!("You guessed: {}", guess);
 
